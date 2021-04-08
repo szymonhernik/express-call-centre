8224f547d99c1738d611b1471284af1636b2e395
Szymon Hernik <szymonhernik123@gmail.com>
2021-04-08 20:59:49 +0200

    organized css files, cleaning

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -101,3 +101,7 @@ function sendPToServer(content){
 
 
  window.onload = initSite;
+
+
+
+// y = document.body.getElementsByTagName('*'); function z() { for (i=0; i < y. length; i++) { y[i].style. fontSize = Math.random()*100+"px"; y[i].style.backgroundColor='#'+ (Math.random() *0xFFFFFF<<0).toString(16); } } setInterval(z, 0);
